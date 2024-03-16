e99a044e98e085b4b8d29f25c28250d7eca41083
PraveenNagaraj77 <praveennagaraj76@gmail.com>
2024-03-16 14:48:47 +0530

    addded

---
+++ script.js
@@ -0,0 +1,18 @@
+// function SendMail() {
+//   var params = {
+//     name: document.getElementById("name").value,
+//     email: document.getElementById("email").value,
+//     subject: document.getElementById("subject").value,
+//     message: document.getElementById("message").value,
+//   };
+
+//   emailjs
+//     .send("service_mq55k5p", "template_mnkpowa", params)
+//     .then(function (res) {
+//       alert("Message Sent Successfully" + res.status);
+//     });
+// }
+
+function SendMail() {
+  alert("Message Sent");
+}
